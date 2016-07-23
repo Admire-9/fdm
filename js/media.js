2f228b2949116784a67d4556e786aa7ae11a5639
unknown <>
2016-07-23 11:41:47 +0800

    delete

--- js/media.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function () {
-  $(".login").css("display","block");
-  $(".cart").css("display","block");
-  $(".login").html("");
-  $(".pull-left").click(function() {
-    $(".topNav").slideToggle();
-  });
-});
